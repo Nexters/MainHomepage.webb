a9aa342ac07c815ab9bf9b0aec6fad90321c190d
jun <leebduk@gmail.com>
2015-08-17 14:17:25 +0900

    add ui router

--- scripts/ActivitiesController.js
+++ scripts/ActivitiesController.js
@@ -1,6 +1,10 @@
 var activitesController = angular.module('activitesController', []);
 
-activitesController.controller('activitesController',function ($scope) {
+activitesController.controller('activitesController','User',function ($scope,User) {
   // body...
   $scope.data="activities";
 })
+
+.factory('User',['$http','$q',function($http,$q){
+
+}])
--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,45 @@
 var app = angular
   .module("app",[
-    'ngRoute',
     'membershipController',
     'activitesController',
-    'contactController'
-  ]).config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-          .when('/',{
-              templateUrl: './templates/About/About.html'
-          })
-          .when('/Activities', {
-              templateUrl: './templates/Activities/Activities.html',
-              controller: 'activitesController'
-          })
-          .when('/Contact', {
-              templateUrl: './templates/Activities/Activities.html',
-              controller: 'contactController'
-          })
-          .when('/Membership', {
-              templateUrl: './templates/Activities/Activities.html',
-              controller: 'membershipController'
-          })
-          .otherwise('/');
-    }]);
+    'contactController',
+    'ui.router'
+  ]).config(['$urlRouterProvider','$stateProvider', function ($urlRouterProvider,$stateProvider) {
+
+      $stateProvider
+        //About
+        .state('About',{
+          url:'/',
+          templateUrl: "templates/About/About.html"
+        })
+
+        //Activities
+        .state('Activities-product',{
+          url:'/Activities-product',
+          templateUrl: "templates/Activities/product.html",
+          controller: 'activitesController'
+        })
+        .state('Activities-member',{
+          url:'/Activities-member',
+          templateUrl: "templates/Activities/member.html"
+        })
+        .state('Activities-notice',{
+          url:'/Activities-notice',
+          templateUrl: "templates/Activities/notice.html"
+        })
+        .state('Activities-media',{
+          url:'/Activities-media',
+          templateUrl: "templates/Activities/media.html"
+        })
+        .state('Activities-memories',{
+          url:'/Activities-memories',
+          templateUrl: "templates/Activities/memories.html"
+        })
+        //Contact
+        .state('Contact',{
+          url:'/Contact',
+          templateUrl: "templates/Activities/Activities.html"
+        })
+
+        $urlRouterProvider.otherwise('/');
+}]);
